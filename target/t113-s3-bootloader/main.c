bcb9dc4e2ea9232e1a4f4b82446e01b9f5dfb125
SG <who.just.the.doctor@gmail.com>
2023-06-22 05:59:21 +0300

    linking, startup

---
+++ target/t113-s3-bootloader/main.c
@@ -0,0 +1,31 @@
+// Cortex-A7/A9 handlers
+void Undef_Handler(void) {
+}
+
+void SWI_Handler(void) {
+}
+
+void PAbort_Handler(void) {
+}
+
+void DAbort_Handler(void) {
+}
+
+void FIQ_Handler(void) {
+}
+
+void Hyp_Handler(void) {
+}
+
+void IRQ_Handler_GIC(void) {
+}
+
+void IRQ_Handler(void) {
+}
+
+void SystemInit(void) {
+}
+
+int main(void) {
+    return 0;
+}
